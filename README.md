10caf5b162da37d797c2399c0c94f9aac6061899
Michika <mhiratsuka00@mylangara.bc.ca>
2017-04-08 03:16:21 +0000

    modified

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # weather app
 ## This is an assignment for week10
 ### The goal of this project
-+ Allow users to enter a location and should return information about the current weather in their given location.
++ users can get weather information in Vancouver.
 + display the information in weather description and the temperature in celsius.
-+ display an weather image.
++ display a weather image.
 
--- app.js
+++ app.js
@@ -1,36 +1,79 @@
-// When  pressing the button, weather info appears.
-function onButtonClick(){
+var btn = document.getElementById('btn');
+var weatherInfo = document.getElementById('display');
+var indxCity = document.wea_place.city_name.selectedIndex;
+var city = document.wea_place.city_name.options[indxCity].text;
 
-//Get data from open weathermap
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=myApi&units=metric', true);
-xhr.Onload = function(){
-	var data = JSON.parse(xhr.responseText); 
-	// console.log(data);
-	var day = 'today';
-	var tag =''
-    tag += '<h2>' + 'Vancouver' + '</h2>';
-    tag += '<table>';
-    for(var i = 0; i<5; i++){
-       	var icon = "<img src='http://openweathermap.org/img/w/" + data.list[i].weather[i].icon + ".png'>";    // weathericon
-        var tempMax = data.list[i].main.temp_max + '℃';   // max temp
-        var tempMin = data.list[i].main.temp_min + '℃';   // min temp
-        var humid = data.list[i].main.humidity +'%'; //humidity
-        var description = data.list[i].weather[i].description; //weather description
-     
-        tag += '<tr>';
-        tag += '<td>' + day + '</td>';
-        tag += '<td>' + icon + '</td>';
-        tag += '<td>Max Temperature</td><td>' + tempMax + '</td>';
-        tag += '<td>Min Temperature</td><td>' + tempMin + '</td>';
-        tag += '<td>Humidity</td><td>' + humid + '</td>';
-        tag += '<td>Desctiption</td><td>' + description + '</td>';
-        tag += '</tr>';
-     }
-    tag += '</table>';
-    var weatherInfo;
-	weatherInfo = document.getElementById('display');
+
+
+ 
+function writeWeather(weatherData){
+	var tag = '';
+	tag += '<h2>' + city + '</h2>';
+		var icon = "<img src='http://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png'>";    // weathericon
+		var when = weatherData.list[0].dt_txt; //date and time
+		var descri = weatherData.list[0].weather[0].description;  //weather description
+		var tempMax = weatherData.list[0].main.temp_max + '℃';   // max temp
+	    var tempMin = weatherData.list[0].main.temp_min + '℃';   // min temp
+	    var humid = weatherData.list[0].main.humidity +'%'; //humidity
+
+		tag += '<td>' + icon + '</td>';
+		tag += '<br>' + when;
+		tag += '<p>';
+		tag += '<br>' + descri;
+		tag += '<br>Max Temperature ' + tempMax;
+	    tag += '<br>Min Temperature ' + tempMin;
+	    tag += '<br>Humidity ' + humid;
+		tag += '<p>';
+
+		var icon = "<img src='http://openweathermap.org/img/w/" + weatherData.list[1].weather[0].icon + ".png'>";    // weathericon
+		var when = weatherData.list[1].dt_txt; //date and time
+		var descri = weatherData.list[1].weather[0].description;  //weather description
+		var tempMax = weatherData.list[1].main.temp_max + '℃';   // max temp
+	    var tempMin = weatherData.list[1].main.temp_min + '℃';   // min temp
+	    var humid = weatherData.list[1].main.humidity +'%'; //humidity
+
+		tag += '<td>' + icon + '</td>';
+		tag += '<br>' + when;
+		tag += '<p>';
+		tag += '<br>' + descri;
+		tag += '<br>Max Temperature ' + tempMax;
+	    tag += '<br>Min Temperature ' + tempMin;
+	    tag += '<br>Humidity ' + humid;
+		tag += '<p>';
+
+		var icon = "<img src='http://openweathermap.org/img/w/" + weatherData.list[2].weather[0].icon + ".png'>";    // weathericon
+		var when = weatherData.list[2].dt_txt; //date and time
+		var descri = weatherData.list[2].weather[0].description;  //weather description
+		var tempMax = weatherData.list[2].main.temp_max + '℃';   // max temp
+	    var tempMin = weatherData.list[2].main.temp_min + '℃';   // min temp
+	    var humid = weatherData.list[2].main.humidity +'%'; //humidity
+
+		tag += '<td>' + icon + '</td>';
+		tag += '<br>' + when;
+		tag += '<p>';
+		tag += '<br>' + descri;
+		tag += '<br>Max Temperature ' + tempMax;
+	    tag += '<br>Min Temperature ' + tempMin;
+	    tag += '<br>Humidity ' + humid;
+		tag += '<p>';
+
 	weatherInfo.innerHTML = tag;
 }
-xhr.send(null);
-}
+
+
+btn.addEventListener('click', (event) =>{
+	event.preventDefault();
+	fetch('http://api.openweathermap.org/data/2.5/forecast?id=6173331&cnt=5&appid=80e55fa739508889fe415e92805fa2b6&units=metric')
+	.then((response) => {
+		if(response.ok){
+			return response.json();
+		} else {
+			throw new Error();
+		}
+	})
+	.then((deta) => 
+		writeWeather(deta))
+	.catch((error) => console.log(error));	
+})
+
+
